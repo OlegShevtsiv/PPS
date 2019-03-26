8336c9a38cf866b8af44d94881a841df62909143
mdzhura <32909428+mdzhura@users.noreply.github.com>
2019-03-27 01:24:50 +0200

    Added Author and User models

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LoginName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime RegistrationDate { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
+
