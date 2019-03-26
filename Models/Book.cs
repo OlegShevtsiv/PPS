6c1207c49b2388b4f8e5edc8c97e6b2fedec07e8
mdzhura <32909428+mdzhura@users.noreply.github.com>
2019-03-27 01:15:05 +0200

    Added Book model

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public decimal Rate { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+    }
+}
+
