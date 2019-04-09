73f2e1e44336a90ba88b42a32f9ff088d9178a68
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2019-04-09 21:15:47 +0300

    Add patern Unit of Work

---
+++ Data/Implementation/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Library.Data.Interfaces;
+
+namespace Library.Data.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LibraryContext _context;
+
+        private Dictionary<Type, object> _repositories;
+
+        public UnitOfWork(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>()
+            where TEntity : class
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Dictionary<Type, object>();
+            }
+
+            Type type = typeof(TEntity);
+            if (!_repositories.ContainsKey(type))
+            {
+                _repositories[type] = new Repository<TEntity>(_context);
+            }
+
+            return (IRepository<TEntity>)_repositories[type];
+        }
+    }
+}
