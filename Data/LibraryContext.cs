585451075fcbff9a4a2b707ba94c72c3fa5bc0f5
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2019-04-09 20:38:05 +0300

    Add data dontext and connection string.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BookStorage> BookStorages { get; set; }
+
+
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -23,9 +23,9 @@ namespace Library
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(connection));
             services.AddMvc();
-            services.AddDbContext<Context>(options =>
-    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
