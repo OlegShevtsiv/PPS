087331508e51cc404b00918eb5c98dd986e2b2f2
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2019-04-10 00:44:33 +0300

    A little changes.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Library.Models;
+using Library.Data;
 
 namespace Library.Controllers
 {
     public class HomeController : Controller
     {
+        LibraryContext db;
+        public HomeController(LibraryContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
             return View();
-        }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page1.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
         }
 
         public IActionResult Error()
---
+++ Data/SampleData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Data
+{
+    public class SampleData
+    {
+        public static void Initialize(LibraryContext context)
+        {
+            // fill database
+
+
+
+            context.SaveChanges();
+        }
+    }
+}
